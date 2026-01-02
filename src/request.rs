4213c1ec4f3faeac13a52c0c0ec21cf4191c6ad8
AfkaraLP <kijo@gmx.net>
2026-01-02 18:45:19 +0100

    get rid of some allocations

--- src/request.rs
+++ src/request.rs
@@ -8,18 +8,19 @@ use crate::method::HttpMethod;
 pub struct Request {
     /// i.e. Get, Post, etc...
     pub method: HttpMethod,
-    /// path, currently including query parameters in the string
+    /// Hath, currently including query parameters in the string
     pub path: String,
     pub headers: HashMap<String, String>,
     pub body: Option<String>,
+    /// Parameter map after '?' in a request
     pub query: Option<HashMap<String, String>>,
 }
 
 impl FromStr for Request {
     type Err = std::io::Error;
 
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let first_line = s
+    fn from_str(input: &str) -> Result<Self, Self::Err> {
+        let mut first_line = input
             .lines()
             .next()
             .ok_or({
@@ -28,48 +29,53 @@ impl FromStr for Request {
                     "failed getting first http line",
                 )
             })?
-            .split_whitespace()
-            .collect::<Vec<&str>>();
-        let (method, path): (HttpMethod, String) = match first_line.as_slice() {
-            [method_str, path, _version] => {
-                (HttpMethod::from_str_val(method_str), (*path).to_string())
-            }
-            _ => {
-                return Err(std::io::Error::new(
-                    std::io::ErrorKind::InvalidInput,
-                    "invalid http top header",
-                ));
-            }
-        };
-        let (path, query): (String, Option<HashMap<String, String>>) = match path.split_once('?') {
+            .split_whitespace();
+
+        let method_str = first_line.next().ok_or(std::io::Error::new(
+            std::io::ErrorKind::InvalidInput,
+            "invalid http top header",
+        ))?;
+
+        let path = first_line.next().ok_or(std::io::Error::new(
+            std::io::ErrorKind::InvalidInput,
+            "invalid http top header",
+        ))?;
+        // TODO: version string is ignored for now
+
+        let method = HttpMethod::from_str_val(method_str);
+
+        let (path, query): (&str, Option<HashMap<String, String>>) = match path.split_once('?') {
             Some((path, query)) => {
                 let query_map: HashMap<String, String> = query
                     .split('&')
                     .filter_map(|q| q.split_once('='))
                     .map(|(k, v)| (k.to_owned(), v.to_owned()))
                     .collect();
-                (path.to_owned(), Some(query_map))
+                (path, Some(query_map))
             }
             None => (path, None),
         };
 
-        let headers: HashMap<String, String> = s
+        let headers: HashMap<String, String> = input
             .lines()
             .take_while(|line| !line.is_empty())
             .skip(1)
             .filter_map(|line| {
                 line.split_once(':')
-                    .map(|(k, v)| (k.trim().to_string(), v.trim().to_string()))
+                    .map(|(k, v)| (k.trim().to_owned(), v.trim().to_owned()))
             })
             .collect();
 
         let body = if method == HttpMethod::Get {
             None
         } else {
-            s.split_once("\r\n\r\n")
-                .map(|v| v.1.to_owned())
-                .filter(|v| !v.is_empty())
+            input
+                .split_once("\r\n\r\n")
+                .map(|head_body_split| head_body_split.1.to_owned())
+                .filter(|body| !body.is_empty())
         };
+
+        let path = path.to_owned();
 
         let req: Request = Request {
             method,
